b69889571485f910599c0751eb1cad4312fd9945
Johan Gawell <johan.gawell@svt.se>
2022-02-24 20:57:03 +0100

    Add styling to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,43 @@
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
 import type { NextPage } from "next";
+import Image from "next/image";
 import Link from "next/link";
-import { PageContainer } from "../components/PageContainer";
 
 const Home: NextPage = () => {
   return (
     <>
-      <PageContainer>
-        <h1>2 Room and a Boom</h1>
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr",
-            columnGap: "16px",
-            rowGap: "16px",
-          }}
-        >
-          <Link href={"/new"}>New game</Link>
-          <Link href={"/join"}>Join game</Link>
-          <button>Role reference</button>
-        </div>
-      </PageContainer>
+      <Container maxWidth="xs" disableGutters>
+        <Image
+          src="/2raab_full_copy.png"
+          height={542}
+          width={629}
+          alt="Two room and a Boom by Allan Gerding and Sean Mccoy"
+        />
+      </Container>
+      <Container maxWidth="xs">
+        <Stack direction="column" alignItems="center" spacing={4} marginTop={4}>
+          <Link href="/new" passHref>
+            <Button size="large" variant="contained" fullWidth>
+              New game
+            </Button>
+          </Link>
+          <Link href="/join" passHref>
+            <Button size="large" variant="outlined" fullWidth>
+              Join game
+            </Button>
+          </Link>
+          <Link
+            href="https://www.ultraboardgames.com/two-rooms-and-a-boom/game-rules.php"
+            passHref
+          >
+            <Button size="large" variant="outlined" fullWidth>
+              Rules
+            </Button>
+          </Link>
+        </Stack>
+      </Container>
     </>
   );
 };
